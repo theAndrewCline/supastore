459c1cc486276afda2cc17fc7a757d3371742a68
Andrew Cline <andrew.cline77@gmail.com>
2021-07-28 07:38:46 -0500

    chore: add formating via prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,38 @@
 <script lang="ts">
-	import Cart from './Cart.svelte'
-	import { cart } from './stores.ts'
-	import { addItemToCart } from './cart/cart.ts'
-	import type {  ShoppingItem } from './shopping-item/index.ts'
+  import Cart from './Cart.svelte'
+  import { cart } from './stores'
+  import { addItemToCart } from './cart/cart'
+  import type { InventoryItem } from './inventory/index'
 
-	const dummy_item: ShoppingItem = {
-		id: 'dummy_item',
-		title: 'Nike Shoes',
-		description: 'Best dang shoes',
-		price: 200,
-		stock: 150
-	}
+  const dummy_item: InventoryItem = {
+    id: 'dummy_item',
+    title: 'Nike Shoes',
+    description: 'Best dang shoes',
+    price: 200,
+    stock: 150
+  }
 
-	const handleAddItemClick = () => {
-		cart.update((current_cart) => addItemToCart(dummy_item, current_cart))
-	}
-
-
+  const handleAddItemClick = () => {
+    cart.update((current_cart) => addItemToCart(dummy_item, current_cart))
+  }
 </script>
 
 <main>
-	<Cart />
-	<button on:click={handleAddItemClick}>Add Dummy Item</button>
+  <Cart />
+  <button on:click={handleAddItemClick}>Add Dummy Item</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/Cart.svelte
+++ src/Cart.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-	import { cart } from './stores.ts'
-	import type { Cart } from './cart/cart.ts'
+  import { cart } from './stores.ts'
+  import type { Cart } from './cart/cart.ts'
 
-	let cart_state: Cart
+  let cart_state: Cart
 
-	const unsubscribe = cart.subscribe((state) => 
-		cart_state = state
-	)
+  const unsubscribe = cart.subscribe((state) => (cart_state = state))
 </script>
 
 <div class="container">
-	<h1>Cart</h1>
-	<ul>
-		{#each cart_state.items as item}
-			<li>{item.title} - {item.quantity} x ${item.price}</li>
-		{/each}
-	</ul>
+  <h1>Cart</h1>
+  <ul>
+    {#each cart_state.items as item}
+      <li>{item.title} - {item.quantity} x ${item.price}</li>
+    {/each}
+  </ul>
 </div>
 
 <style>
-	.container {
-		background: #e9e9e9;
-		border-radius: 5px;
-		box-shadow: 1px 1px 2px rgba(0, 0, 0, .3);
-		padding-top: .25rem;
-		padding-bottom: .25rem;
-	}
+  .container {
+    background: #e9e9e9;
+    border-radius: 5px;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
+  }
 
-	ul {
-		list-style: none;
-	}
+  ul {
+    list-style: none;
+  }
 
-	li {
-		background: #FFFFFF;
-		border-radius: 5px;
-		margin-bottom: 8px;
-	}
+  li {
+    background: #ffffff;
+    border-radius: 5px;
+    margin-bottom: 8px;
+  }
 </style>
